8b1ac24a91c9cf8f992affc2fb370026ce5002eb
Ashutosh Jha <aj97389@gmail.com>
2019-08-27 00:30:18 +0530

    Secret key to bash

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,8 +6,8 @@ from flask_login import LoginManager
 from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 bcrypt  = Bcrypt(app)
 login_manager = LoginManager(app)
