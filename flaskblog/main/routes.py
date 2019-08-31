a0d87c3fb3c9d242cb4abe51a3d4c69de84c5116
Ashutosh Jha <aj97389@gmail.com>
2019-08-31 20:12:14 +0530

    per page post from 5 to 4

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -9,7 +9,7 @@ main = Blueprint('main', __name__)
 def home():
     page = request.args.get('page', 1, type=int)
     page2 = request.args.get('page2', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=4)
     users = User.query.paginate(page=page2)
     return render_template('home.html', posts=posts, users=users)
 
