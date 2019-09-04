aa37c7ca9080c15d9481c89f67dc47d74f95bf15
Ashutosh Jha <aj97389@gmail.com>
2019-09-04 22:04:56 +0530

    minor chages

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, validators
 from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired(),  validators.length(max=30)])
+    content = TextAreaField('Content', validators=[DataRequired(),  validators.length(max=400)])
     submit = SubmitField('Post')
 
 
